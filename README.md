ad1ead17e1b6576f886a2a384f8eb4a0483b9130
Crash285 <jackadam852@gmail.com>
2024-08-19 17:26:44 +0200

    index corrupted...

--- README.md
+++
@@ -1,3 +0,0 @@
-# Silksong Alarm
-
-An mobile application that fetches the newest Daily Silksong News and plays it back as an alarm.
--- android/app/src/main/kotlin/com/example/silksong_alarm/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.silksong_alarm
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
